3c5686369d02881d787537aaa05c684a1180887b
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-11 07:16:03 +0530

    Modifying the PlacesList Component and using it in AllPlaces file

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,13 +1,34 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
+  {
+    /*We need to handle null error*/
+  }
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallBackContainer}>
+        <Text style={styles.fallBackText}>No places added yet</Text>
+      </View>
+    );
+  }
   return (
     <FlatList
       data={places}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <PlaceItem place={item}/>}
+      renderItem={({ item }) => <PlaceItem place={item} />}
     />
   );
 }
 export default PlacesList;
+
+const styles = StyleSheet.create({
+    fallBackContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    fallBackText:{
+       fontSize:16,
+    }
+});
