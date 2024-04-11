f10b27ed1153b8fce2b28c0b1ee3bcc5a46df5a2
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-11 07:06:55 +0530

    Creating a new component PlaceItem and using it in the list to display

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,6 +1,13 @@
 import { FlatList } from "react-native";
+import PlaceItem from "./PlaceItem";
 
-function PlacesList({places}){
-    return <FlatList data={places} />
+function PlacesList({ places }) {
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item}/>}
+    />
+  );
 }
 export default PlacesList;
