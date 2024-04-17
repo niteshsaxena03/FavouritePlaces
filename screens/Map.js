e20342dbd0b16c22021cedc6a5480057c9e92ef9
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-17 13:47:52 +0530

    Adding a logic to save picked location marked on the map

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useState, useLayoutEffect,useCallback } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../components/UI/IconButton";
 
-function Map() {
+function Map({ navigation }) {
   const [selectedLocation, setSelectedLocation] = useState();
 
   const region = {
@@ -18,6 +19,29 @@ function Map() {
 
     setSelectedLocation({ lat: lat, lng: lng });
   }
+  const savePickedLocationHandler=useCallback(() =>{//this useCallBack will prevent unnecessary reloads
+    if (!selectedLocation) {
+      Alert.alert("No location picked", "You must pick a location first");
+      return;
+    }
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  },[navigation,selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation,savePickedLocationHandler]);
 
   return (
     <MapView
