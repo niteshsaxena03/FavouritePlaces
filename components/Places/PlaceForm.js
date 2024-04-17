8cad18d69bc277cabf60aff8a52713e17f809f6f
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-17 15:39:31 +0530

    Creating a custom button component and using it in place form

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -3,12 +3,16 @@ import { useState } from "react";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
+import Button from "../UI/Button";
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
 
   function changeTitleHandler(enteredTitle) {
     setEnteredTitle(enteredTitle);
+  }
+  function savePlaceHandler(){
+
   }
   return (
     <ScrollView style={styles.form}>
@@ -18,6 +22,7 @@ function PlaceForm() {
       </View>
       <ImagePicker/>
       <LocationPicker/>
+      <Button onPress={savePlaceHandler}>Add Place</Button>
     </ScrollView>
   );
 }
