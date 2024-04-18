6b16ceb42c8b64889c0b52c4cb8ad0fd08e7bb68
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-18 16:48:23 +0530

    Adding styling to place item and list

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,36 +1,42 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import PlaceItem from "./PlaceItem";
+
 import { Colors } from "../../constants/colors";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
-  {
-    /*We need to handle null error*/
-  }
   if (!places || places.length === 0) {
     return (
-      <View style={styles.fallBackContainer}>
-        <Text style={styles.fallBackText}>No places added yet</Text>
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
       </View>
     );
   }
+
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
     />
   );
 }
+
 export default PlacesList;
 
 const styles = StyleSheet.create({
-    fallBackContainer:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    fallBackText:{
-       fontSize:16,
-       color:Colors.primary200
-    }
+  list: {
+    margin: 24,
+  },
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+    color: Colors.primary200,
+  },
 });
