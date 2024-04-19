bb938834a7e74b21500c0670e5b98440464d6a6a
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-19 13:09:57 +0530

    Showing the map for location

--- screens/Map.js
+++ screens/Map.js
@@ -1,36 +1,53 @@
-import { useState, useLayoutEffect,useCallback } from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+
 import IconButton from "../components/UI/IconButton";
 
-function Map({ navigation }) {
-  const [selectedLocation, setSelectedLocation] = useState();
+function Map({ navigation, route }) {
+  const initialLocation = route.params && {
+    lat: route.params.initialLat,
+    lng: route.params.initialLng,
+  };
+
+  const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
   const region = {
-    latitude: 37.78,
-    longitude: -122.43,
+    latitude: initialLocation ? initialLocation.lat : 37.78,
+    longitude: initialLocation ? initialLocation.lng : -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
 
   function selectLocationHandler(event) {
+    if (initialLocation) {
+      return;
+    }
     const lat = event.nativeEvent.coordinate.latitude;
     const lng = event.nativeEvent.coordinate.longitude;
 
     setSelectedLocation({ lat: lat, lng: lng });
   }
-  const savePickedLocationHandler=useCallback(() =>{//this useCallBack will prevent unnecessary reloads
+
+  const savePickedLocationHandler = useCallback(() => {
     if (!selectedLocation) {
-      Alert.alert("No location picked", "You must pick a location first");
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location (by tapping on the map) first!"
+      );
       return;
     }
+
     navigation.navigate("AddPlace", {
       pickedLat: selectedLocation.lat,
       pickedLng: selectedLocation.lng,
     });
-  },[navigation,selectedLocation]);
+  }, [navigation, selectedLocation]);
 
   useLayoutEffect(() => {
+    if (initialLocation) {
+      return;
+    }
     navigation.setOptions({
       headerRight: ({ tintColor }) => (
         <IconButton
@@ -41,7 +58,7 @@ function Map({ navigation }) {
         />
       ),
     });
-  }, [navigation,savePickedLocationHandler]);
+  }, [navigation, savePickedLocationHandler, initialLocation]);
 
   return (
     <MapView
