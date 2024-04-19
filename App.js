586ed0b55db66f48d8e10c76a854ddf736a3caf2
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-04-19 12:24:51 +0530

    Initializing a database for this app using SQL

--- App.js
+++ App.js
@@ -1,42 +1,69 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
+
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
-import {Colors} from './constants/colors';
+import { Colors } from "./constants/colors";
 import Map from "./screens/Map";
+import { init } from "./util/database";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle:{backgroundColor:Colors.primary500},
-          headerTintColor:Colors.gray700,
-          contentStyle:{backgroundColor:Colors.gray700}
-        }}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
-              title:'Your Favourite Places',
+              title: "Your Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
+                  icon="add"
                   size={24}
                   color={tintColor}
-                  icon="add"
                   onPress={() => navigation.navigate("AddPlace")}
                 />
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} options={{title:'Add a new place'}}/>
-          <Stack.Screen name="Map" component={Map}/>
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
